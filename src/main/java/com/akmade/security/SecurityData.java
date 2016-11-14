3ca43f68cdcb4f94f961b0726437c9885e97c40c
Brian Lieb <brianlieb@hotmail.com>
2016-11-13 17:36:28 -0900

    Committing my last chnages for the evening.

--- src/main/java/com/akmade/security/SecurityData.java
+++ src/main/java/com/akmade/security/SecurityData.java
@@ -7,6 +7,7 @@ import static com.akmade.messaging.Utility.invalidateMessage;
 
 import java.util.Collection;
 import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 import com.akmade.messaging.api.Messenger.MessageAction;
@@ -17,8 +18,10 @@ import com.akmade.messaging.api.senders.demoperson.SecurityQueues;
 import com.akmade.security.repositories.CommandRepo;
 import com.akmade.security.repositories.QueryRepo;
 import com.google.protobuf.Any;
+import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.Message;
 import com.akmade.messaging.dto.MessagingDTO.BusMessage;
+import com.akmade.messaging.security.dto.SecurityDTO;
 
 import javax.jms.JMSException;
 
@@ -29,44 +32,65 @@ public class SecurityData extends ServiceController {
 	QueryRepo qryRepo = new QueryRepo();
 	CommandRepo cmdRepo = new CommandRepo();
 	
-	
-	private Function<Message, com.google.protobuf.Any> makeAny =
-		object ->  Any.pack(object);
-		
-		
-	private Function<Collection<? extends Message>, Collection<com.google.protobuf.Any>> makeAnies =
+	private Function<Collection<? extends Message>, Collection<com.google.protobuf.Any>> makeAnys =
 		objects ->  objects
 						.stream()
 						.map(o -> Any.pack(o))
 						.collect(Collectors.toList());
+		
+
+		
+	private <T extends Message> BusMessage makeNewBusMessage(BusMessage msg, T object, String errMsg) {
+		try {
+			return BusMessage.newBuilder()
+					.addObjects(Any.pack(object))
+					.setCommand("Reply: " + msg.getCommand())
+					.setStatus(msg.getStatus())
+					.build();
+		} catch (Exception e) {
+			log.error(errMsg, e);
+			return invalidateMessage(msg, e, errMsg);
+		}	
+	}
+		
+	
+	private BusMessage makeNewBusMessage(BusMessage msg, Collection<? extends Message> objects, String errMsg) {
+		try {
+			return BusMessage.newBuilder()
+					.addAllObjects(makeAnys.apply(objects))
+					.setCommand("Reply: " + msg.getCommand())
+					.setStatus(msg.getStatus())
+					.build();
+		} catch (Exception e) {
+			log.error(errMsg, e);
+			return invalidateMessage(msg, e, errMsg);
+		}
+	}
+	
+	
+	private Collection<? extends Message> getDTOFromMessage(BusMessage msg) {
+		return msg
+				.getObjectsList()
+				.stream()
+				.map(o -> {
+					try {
+						return o.unpack(SecurityDTO.Account.class);
+					} catch (InvalidProtocolBufferException e) {
+						e.printStackTrace();
+						return null;
+					}
+				})
+				.collect(Collectors.toList());
+	}
+	
+	private final MessageAction getAccountByUsername = 
+			msg -> makeNewBusMessage(msg, qryRepo.getAccountById.apply(0).get(), "Error getting the account");
 	 
 	private final MessageAction getAccounts = 
-			m -> {
-				try {
-					return BusMessage.newBuilder()
-							.addAllObjects(makeAnies.apply(qryRepo.getAccounts.get()))
-							.setCommand("Reply: " + m.getCommand())
-							.setStatus(m.getStatus())
-							.build();
-				} catch (Exception e) {
-					log.error("Error finding people", e);
-					return invalidateMessage(m, e, "Security data encountered error getting address types.");
-				}
-			};
+			m -> makeNewBusMessage(m, qryRepo.getAccounts.get(), "Error getting the accounts.");
 			
 	private final MessageAction getAddressTypes = 
-		m -> {
-			try {
-				return BusMessage.newBuilder()
-						.addAllObjects(makeAnies.apply(qryRepo.getAddressTypes.get()))
-						.setCommand("Reply: " + m.getCommand())
-						.setStatus(m.getStatus())
-						.build();
-			} catch (Exception e) {
-				log.error("Error finding people", e);
-				return invalidateMessage(m, e, "Security data encountered error getting address types.");
-			}
-		};
+		m -> makeNewBusMessage(m, qryRepo.getAddressTypes.get(), "Error getting the address types.");
 	
 	private MessageProtocol mProtocol = 
 			m -> {
